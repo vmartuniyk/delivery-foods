7b40483e466dacd447bb3597243c20c92e031fa2
Volodimir Martuniyk <volodimir.martuniyk@gmail.com>
2020-04-10 22:40:51 +0300

    Add modal window and animated elements

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const shoppingCart = document.querySelector("#shopping-cart");
+const modal= document.querySelector("#modal");
+const close= document.querySelector(".close");
+
+
+shoppingCart.addEventListener("click", toggleModal);
+
+close.addEventListener("click", toggleModal);
+
+function toggleModal() {
+    modal.classList.toggle('is-open');
+}
+
+new WOW().init();
